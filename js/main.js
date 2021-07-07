d676dc219d9532df88300204cb35790b0ade6586
Yhuri-Gross <yhurigross@gmail.com>
2021-07-07 15:43:06 -0300

    Exercícios

---
+++ js/main.js
@@ -0,0 +1,88 @@
+/*function clicou (){
+    document.getElementById ("agradecimento").innerHTML = "Obrigado por clicar";
+
+} 
+
+function redirecionar (){
+    window.open ("https://www.google.com.br/")
+}
+
+/*function trocar(elemento){
+    /*document.getElementById ("mouseMove").innerHTML = "Obrigado por passar o mouse";
+    alert("Troquei o texto")*/
+    /*elemento.innerHTML = "Obrigado por passar o mouse"
+}
+
+function voltar(elemento){
+    /*document.getElementById ("mouseMove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = " Passe o mouse aqui"
+}*/
+/*console.log (document.getElementById ("agradecimento"))
+    /*alert ("Obrigado po clicar");
+
+
+/*function soma (n1, n2){
+    return n1 + n2
+}
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+    validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual a sua idade ? ");
+console.log (validaIdade(idade));
+/*function setReplace (frase, nome, novo_nome){
+    return frase.replace (nome, novo_nome)
+}
+
+alert (soma(5, 10));
+alert (setReplace("vai Japão", "Japão", "Brasil"));*/
+
+/*var d = new Date ();
+alert (d);
+alert (d.getDay());
+alert (d.getHours());
+alert (d.getMinutes());
+*/
+
+/*var count;
+for(count = 0; count <= 5; count++){
+    alert (count)
+};*/
+
+/*var count = 0
+while (count < 10){
+    console.log (count);
+    count = count + 1;
+}*/
+
+/*var idade = prompt ("Qual a sua idade ? ")
+if (idade >= 18){
+    alert ("Você é maior de idade");
+} else {
+    alert ("Você é menor de idade")
+};*/
+
+/*var lista = ["maça", "Pera", "Banana"];
+lista.push ("Uva")
+console.log (lista);
+console.log (lista);
+console.log (lista.toString());
+console.log (lista.join(" - "));*/
+
+
+/*var nome = "Yhuri Gross";
+var idade = 23;
+var idade2 = 199
+var frase = "Japão é o melhor time do mundo"
+alert (nome + " tem " + idade + " anos ");
+alert (idade + idade2);
+console.log (nome)
+console.log (idade + idade2)
+console.log (frase.replace ("Japão", "Brasil"));*/
